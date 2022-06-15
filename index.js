d5736634c349133822ece2bb70fd604c850fa7f0
Sil <silveltman@hotmail.com>
2022-06-15 14:47:20 +0200

    c

---
+++ index.js
@@ -0,0 +1,42 @@
+const plugin = require('tailwindcss/plugin')
+const flattenColorPalette = require('tailwindcss/src/util/flattenColorPalette')
+
+const textBorder = plugin(
+  function ({ matchUtilities, theme, config, e }) {
+    const textBorderWidth = `--tw${config('prefix')}-text-border-width`
+    const pos = `var(${textBorderWidth},1px)`
+    const neg = `calc(var(${textBorderWidth}, 1px) * -1)`
+
+    matchUtilities(
+      {
+        'text-border': (value) => ({
+          'text-shadow': `
+            ${pos} ${pos} 0 ${value},
+            ${neg} ${neg} 0 ${value},
+            ${pos} ${neg} 0 ${value},
+            ${neg} ${pos} ${value};
+          `,
+        }),
+      },
+      {
+        values: flattenColorPalette(theme('colors')),
+        type: ['color', 'any'],
+      }
+    )
+
+    matchUtilities(
+      {
+        'text-border': (value) => ({
+          [textBorderWidth]: value
+        }), 
+      },
+      { values: theme('borderWidth') }
+    ) 
+  },
+)
+
+module.exports = {
+  plugins: [
+    textBorder
+  ],
+}
