627fcb31b8ea954e99f6813ca020d3290fd88f2f
Sil <silveltman@hotmail.com>
2022-06-15 15:23:13 +0200

    fixed export line

--- index.js
+++ index.js
@@ -1,42 +1,36 @@
 const plugin = require('tailwindcss/plugin')
 const flattenColorPalette = require('tailwindcss/src/util/flattenColorPalette')
 
-const textBorder = plugin(
-  function ({ matchUtilities, theme, config, e }) {
-    const textBorderWidth = `--tw${config('prefix')}-text-border-width`
-    const pos = `var(${textBorderWidth},1px)`
-    const neg = `calc(var(${textBorderWidth}, 1px) * -1)`
+const textBorder = plugin(function ({ matchUtilities, theme, config }) {
+  const textBorderWidth = `--tw${config('prefix')}-text-border-width`
+  const pos = `var(${textBorderWidth},1px)`
+  const neg = `calc(var(${textBorderWidth}, 1px) * -1)`
 
-    matchUtilities(
-      {
-        'text-border': (value) => ({
-          'text-shadow': `
+  matchUtilities(
+    {
+      'text-border': (value) => ({
+        'text-shadow': `
             ${pos} ${pos} 0 ${value},
             ${neg} ${neg} 0 ${value},
             ${pos} ${neg} 0 ${value},
             ${neg} ${pos} ${value};
           `,
-        }),
-      },
-      {
-        values: flattenColorPalette(theme('colors')),
-        type: ['color', 'any'],
-      }
-    )
+      }),
+    },
+    {
+      values: flattenColorPalette(theme('colors')),
+      type: ['color', 'any'],
+    }
+  )
 
-    matchUtilities(
-      {
-        'text-border': (value) => ({
-          [textBorderWidth]: value
-        }), 
-      },
-      { values: theme('borderWidth') }
-    ) 
-  },
-)
+  matchUtilities(
+    {
+      'text-border': (value) => ({
+        [textBorderWidth]: value,
+      }),
+    },
+    { values: theme('borderWidth') }
+  )
+})
 
-module.exports = {
-  plugins: [
-    textBorder
-  ],
-}
+module.exports = textBorder
